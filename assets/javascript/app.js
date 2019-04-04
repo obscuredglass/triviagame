7ee5257d28b51aa459576f7aba1a35ca63babd9d
ahochman <ahochman2019@gmail.com>
2019-04-04 17:26:41 -0400

    beginning of questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+// questions and answers
+
+var questions = [
+{
+  question: 'Who is the singer of The Talking Heads?'
+  answers: [
+    answer: 'A.'
+    answer: 'B.'
+    answer: 'C.'
+    answer: 'D.'
+  ]
+},
+ {
+  question: 'Who is the original singer for Metallica?'
+
+  answers: [
+    answer: 'A.'
+    answer: 'B.'
+    answer: 'C.'
+    answer: 'D.'
+  ]
+},
+
+{
+
+  question: 'What year was Truckin by The Grateful Dead released?'
+
+  answers: [
+    answer: 'A.'
+    answer: 'B.'
+    answer: 'C.'
+    answer: 'D.'
+  ]
+
+}
+
+{
+  question: 'How was Otis Redding, Buddy Holly, and most of Lynryd Skynyrd killed?'
+
+  answers: [
+    answer: 'A.'
+    answer: 'B.'
+    answer: 'C.'
+    answer: 'D.'
+  ]
+
+
+}
+];
+
+
+// global variables
+var game;
+var counter = 0;
+var clock;
+var timer = 30;
+var correctCounter = 0;

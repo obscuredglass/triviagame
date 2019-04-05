9f05d5b7cdf22016d5fb0d9731e0208e4f01a9c1
ahochman <ahochman2019@gmail.com>
2019-04-05 18:51:00 -0400

    some big corrections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,145 @@
-// questions and answers
+var triviaQuestions = [{
+	question: "What is Flying Lotus' real name?",
+	answerList: ["Steve Ellison", "Hunter Bascko", "Jessie Jackson"],
+	answer: 0
+},{
+	question: "What famous musician is he related to?",
+	answerList: ["Samwise Gangi", "Alice Coltrane", "James Brown"],
+	answer: 1
+},{
+	question: "What is the name of his first album?",
+	answerList: ["0000", "1955", "1983"],
+	answer: 2
+},{
+	question: "What late night network uses a lot of his music?",
+	answerList: ["Adult Swim", "HBO AfterDark", "Encore Action"],
+	answer: 0
+},{
+	question: "What independant hip-hop label did he work for before rising to fame?",
+	answerList: ["Stones Throw Records", "Digi Crates", "Duck Down Music"],
+	answer: 0
+},{
+	question: "What record label was he signed to that is also the home for Boards of Canada and Aphex Twin?",
+	answerList: ["Hotflush Recordings", "Warp Records", "LuckyMe"],
+	answer: 1
+}];
 
-var questions = [
-{
-  question: 'Who is the singer of The Talking Heads?'
-  answers: [
-    answer: 'A.'
-    answer: 'B.'
-    answer: 'C.'
-    answer: 'D.'
-  ]
-},
- {
-  question: 'Who is the original singer for Metallica?'
-
-  answers: [
-    answer: 'A.'
-    answer: 'B.'
-    answer: 'C.'
-    answer: 'D.'
-  ]
-},
-
-{
-
-  question: 'What year was Truckin by The Grateful Dead released?'
-
-  answers: [
-    answer: 'A.'
-    answer: 'B.'
-    answer: 'C.'
-    answer: 'D.'
-  ]
-
+var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6'];
+var currentQuestion; var correctAnswer; var incorrectAnswer; var unanswered; var seconds; var time; var answered; var userSelect;
+var messages = {
+	correct: "You got it!",
+	incorrect: "Booooo.",
+	endTime: "You goofed!",
+	finished: "Let's count them up."
 }
 
-{
-  question: 'How was Otis Redding, Buddy Holly, and most of Lynryd Skynyrd killed?'
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
 
-  answers: [
-    answer: 'A.'
-    answer: 'B.'
-    answer: 'C.'
-    answer: 'D.'
-  ]
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
 
-
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
 }
-];
 
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	answered = true;
+	
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
 
-// global variables
-var game;
-var counter = 0;
-var clock;
-var timer = 30;
-var correctCounter = 0;
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+	$('#gif').html('<img src = "assets/images/'+ gifArray[currentQuestion] +'.gif" width = "400px">');
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
+}
